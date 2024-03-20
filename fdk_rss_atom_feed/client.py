3f68f33d91a069912861ad32fe7b4a7d2f575c9d
Terje Sylvarnes <108797958+terjesyl@users.noreply.github.com>
2024-03-20 13:56:28 +0100

    chore: handle running locally without TLS

--- fdk_rss_atom_feed/client.py
+++ fdk_rss_atom_feed/client.py
@@ -9,21 +9,31 @@ from elasticsearch import Elasticsearch
 class Client:
     def __init__(self) -> None:
         elastic_host = os.getenv("ELASTIC_HOST", "http://localhost:9200")
-        elastic_user = os.getenv("ELASTIC_USER")
-        elastic_pwd = os.getenv("ELASTIC_PASSWORD")
-        elastic_ca_cert = os.getenv("ELASTIC_CA_CERT")
+        elastic_user = os.getenv("ELASTIC_USER", "")
+        elastic_pwd = os.getenv("ELASTIC_PASSWORD", "")
+        elastic_ca_cert = os.getenv("ELASTIC_CA_CERT", None)
 
-        try:
+        if elastic_ca_cert is None:
+            logging.warning(
+                "No ELASTIC_CA_CERT environment variable set. "
+                "This is only safe for development and testing."
+            )
             self.es = Elasticsearch(
                 hosts=[f"{elastic_host}"],
-                http_auth=(elastic_user, elastic_pwd),
-                use_ssl=True,
-                verify_certs=True,
-                ca_certs=f"{elastic_ca_cert}",
+                basic_auth=(elastic_user, elastic_pwd),
             )
-        except Exception as e:
-            logging.error(f"Failed to connect to Elasticsearch: {e}")
-            raise
+        else:
+            logging.warning("Found certificate")
+            try:
+                self.es = Elasticsearch(
+                    hosts=[f"{elastic_host}"],
+                    basic_auth=(elastic_user, elastic_pwd),
+                    verify_certs=True,
+                    ca_certs=f"{elastic_ca_cert}",
+                )
+            except Exception as e:
+                logging.error(f"Failed to connect to Elasticsearch: {e}")
+                raise
 
     def search(self, query: dict[str, Any]) -> ObjectApiResponse[Any]:
         return self.es.search(index="datasets", **query)
