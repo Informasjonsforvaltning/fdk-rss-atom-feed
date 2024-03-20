ad7abbac0469e9c840075bff75814b49f0724301
Terje Sylvarnes <108797958+terjesyl@users.noreply.github.com>
2024-03-20 09:29:32 +0100

    chore: connect to new elastic search

--- fdk_rss_atom_feed/client.py
+++ fdk_rss_atom_feed/client.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Any
 
@@ -7,9 +8,22 @@ from elasticsearch import Elasticsearch
 
 class Client:
     def __init__(self) -> None:
-        host = os.getenv("ELASTIC_HOST", "http://localhost")
-        port = os.getenv("ELASTIC_PORT", "9200")
-        self.es = Elasticsearch([f"{host}:{port}"])
+        elastic_host = os.getenv("ELASTIC_HOST", "http://localhost:9200")
+        elastic_user = os.getenv("ELASTIC_USER")
+        elastic_pwd = os.getenv("ELASTIC_PASSWORD")
+        elastic_ca_cert = os.getenv("ELASTIC_CA_CERT")
+
+        try:
+            self.es = Elasticsearch(
+                hosts=[f"{elastic_host}"],
+                http_auth=(elastic_user, elastic_pwd),
+                use_ssl=True,
+                verify_certs=True,
+                ca_certs=f"{elastic_ca_cert}",
+            )
+        except Exception as e:
+            logging.error(f"Failed to connect to Elasticsearch: {e}")
+            raise
 
     def search(self, query: dict[str, Any]) -> ObjectApiResponse[Any]:
         return self.es.search(index="datasets", **query)
